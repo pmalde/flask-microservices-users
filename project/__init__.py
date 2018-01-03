47a815132d39c87302cc4dc3ec30ed6d6ee89a90
greaseoyster <preyesh.malde@gmail.com>
2018-01-03 21:36:54 +0000

    Added docker files and db

--- project/__init__.py
+++ project/__init__.py
@@ -1,5 +1,7 @@
 import os
+import datetime
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 # Instantiate the App
 app = Flask(__name__)
@@ -8,7 +10,26 @@ app = Flask(__name__)
 app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object('project.config.DevelopmentConfig')
 
+# instantiate the db
+db = SQLAlchemy(app)
 
+
+# model
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=False, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+        self.created_at = datetime.datetime.utcnow()
+
+
+# Routes
 @app.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify({
